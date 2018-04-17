ea83963d2cbb4c72331e5a898f2ba7b90886bd67
Tobias Salzmann <salzmann@talon.one>
2018-04-17 10:04:58 +0200

    Added benchmarking

--- tap_test.go
+++ tap_test.go
@@ -8,6 +8,7 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/require"
+	"github.com/valyala/fasthttp"
 	"go.uber.org/zap"
 )
 
@@ -45,4 +46,31 @@ func TestAPICallWithHMAC(t *testing.T) {
 	res, err := server.Client().Do(req)
 	require.NoError(t, err)
 	require.Equal(t, http.StatusOK, res.StatusCode)
+	require.Equal(t, Version, res.Header.Get("X-Tap"))
+}
+
+func BenchmarkTap(b *testing.B) {
+	server := httptest.NewServer(nil)
+	defer server.Close()
+
+	logger, err := zap.NewProduction()
+	require.NoError(b, err)
+	tap, err := New(Config{
+		TalonAPI: server.URL,
+		Logger:   logger,
+	})
+	require.NoError(b, err)
+	defer tap.Close()
+
+	tapServer := httptest.NewServer(tap.Handler())
+	defer tapServer.Close()
+
+	c := &fasthttp.HostClient{
+		Addr: tapServer.Listener.Addr().String(),
+	}
+	for i := 0; i < b.N; i++ {
+		statusCode, _, err := c.Get(nil, tapServer.URL)
+		require.NoError(b, err)
+		require.Equal(b, http.StatusNotFound, statusCode)
+	}
 }
