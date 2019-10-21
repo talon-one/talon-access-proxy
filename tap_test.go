b750477d5c7dd366753057a423df34547f6cfe02
Oliver Fish <oliver.fish@talon.one>
2019-10-21 13:14:13 +0200

    Removed old test

--- tap_test.go
+++ tap_test.go
@@ -1,8 +1,6 @@
 package talon_access_proxy
 
 import (
-	"bytes"
-	"encoding/json"
 	"net/http"
 	"net/http/httptest"
 	"testing"
@@ -11,43 +9,6 @@ import (
 	"github.com/valyala/fasthttp"
 	"go.uber.org/zap"
 )
-
-func TestAPICallWithHMAC(t *testing.T) {
-	logger, err := zap.NewDevelopment()
-	require.NoError(t, err)
-	// from the https://developers.talon.one/docs/
-	tap, err := New(Config{
-		TalonAPI: "https://demo.talon.one",
-		Application: map[string]*ApplicationConfig{
-			"73": &ApplicationConfig{
-				CalculateHMAC:  true,
-				ApplicationKey: "e3b620ed8144f292",
-			},
-		},
-		Logger: logger,
-	})
-	require.NoError(t, err)
-
-	server := httptest.NewServer(tap.Handler())
-
-	defer server.Close()
-
-	var buffer bytes.Buffer
-	require.NoError(t, json.NewEncoder(&buffer).Encode(map[string]interface{}{
-		"attributes": map[string]interface{}{
-			"Email": "carltonb@hushmail.com",
-		},
-	}))
-
-	req, err := http.NewRequest("PUT", server.URL+"/v1/customer_profiles/165f239c", &buffer)
-	require.NoError(t, err)
-	req.Header.Set("Content-Signature", "signer=73")
-	req.Header.Set("Content-Type", "application/json")
-	res, err := server.Client().Do(req)
-	require.NoError(t, err)
-	require.Equal(t, http.StatusOK, res.StatusCode)
-	require.Equal(t, Version, res.Header.Get("X-Tap"))
-}
 
 func BenchmarkTap(b *testing.B) {
 	server := httptest.NewServer(nil)
